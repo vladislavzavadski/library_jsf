5deb7836cd971375cdd68bec821523bf94901282
Vlad <vladislav.zavadski@gmail.com>
2016-03-10 22:08:15 +0200

    add authorization and authentication

--- src/java/com/library/usebean/User.java
+++ src/java/com/library/usebean/User.java
@@ -5,8 +5,18 @@
  */
 package com.library.usebean;
 
+import java.io.IOException;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
 
 /**
  *
@@ -20,6 +30,15 @@ public class User {
      * Creates a new instance of User
      */
     private String name;
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }  
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
     
     public User() {
     }
@@ -32,5 +51,46 @@ public class User {
         this.name = name;
     }
     
+    public void login(){
+        HttpServletRequest httpServletRequest = (HttpServletRequest)FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            if(FacesContext.getCurrentInstance().getExternalContext().getRemoteUser()!=null){
+               httpServletRequest.logout();
+            }
+            httpServletRequest.login(name, password);
+             HttpServletResponseWrapper wrappedResponse = new HttpServletResponseWrapper(((HttpServletResponse)FacesContext.getCurrentInstance().getExternalContext().getResponse()));
+            try {
+                wrappedResponse.sendRedirect(httpServletRequest.getContextPath()+"/pages/books.xhtml");
+            } catch (IOException ex) {
+                Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+            }
+           
+        } catch (ServletException ex) {
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+            ResourceBundle bundle = ResourceBundle.getBundle("com.library.properties.messages", FacesContext.getCurrentInstance().getViewRoot().getLocale());
+            FacesMessage facesMessage = new FacesMessage(bundle.getString("invalid_authorisation"));           
+            facesMessage.setSeverity(FacesMessage.SEVERITY_ERROR);
+            FacesContext.getCurrentInstance().addMessage("login_form", facesMessage);
     
+        }
+    }
+    public String logout(){
+        try {
+            ((HttpServletRequest)FacesContext.getCurrentInstance().getExternalContext().getRequest()).logout();
+            FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+            return "exit";
+        } catch (ServletException ex) {
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+            return "exit";
+        }
+    }
+    
+    public boolean isUserAdmin(){
+        return FacesContext.getCurrentInstance().getExternalContext().isUserInRole("ADMIN");
+    }
 }
--- web/resources/js/library.js
+++ web/resources/js/library.js
@@ -1,3 +1,16 @@
+function checkValue(form, message) {
+    
+    var userInput = form[form.id + ":username"];
+    
+    if (userInput.value ==''){
+        alert(message)
+        userInput.focus();
+        return false;
+    } 
+    
+    return true;
+}
+
 function showProgress(data) {
     
     if (data.status == "begin") {
